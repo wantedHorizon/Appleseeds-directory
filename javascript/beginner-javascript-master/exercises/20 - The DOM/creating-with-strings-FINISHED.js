9b13f352a76abe0c3658eed2b3bf6b20823206b8
WantedHorizon <eliran.amzalag@gmail.com>
2020-08-25 18:44:27 +0300

    day 23

--- javascript/beginner-javascript-master/exercises/20 - The DOM/creating-with-strings-FINISHED.js
+++ javascript/beginner-javascript-master/exercises/20 - The DOM/creating-with-strings-FINISHED.js
@@ -3,7 +3,8 @@ const item = document.querySelector('.item');
 const width = 500;
 const src = `https://picsum.photos/${width}`;
 const desc = `Cute Pup <img onload="alert('HACKED')" src="https://picsum.photos/50"/>`;
-const myHTML = `
+const myHTML =
+ `
   <div class="wrapper">
     <h2>Cute ${desc}</h2>
     <img src="${src}" alt="${desc}"/>
