f02414ef25426f6cbc2ccfb2d63bd09dce59195c
WantedHorizon <eliran.amzalag@gmail.com>
2020-10-12 12:39:22 +0300

    day 50

--- React/48/netflix-youtube/src/hooks/index.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export { default as useAuthListener } from './user-auth-listener';
-export { default as useContent } from './use-content';
--- React/48/netflix-youtube/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './home/Home';
-export { default as Signin } from './signin/Signin';
-export { default as Signup } from './signup/Signup';
-export { default as Browse } from './browse/Browse';
